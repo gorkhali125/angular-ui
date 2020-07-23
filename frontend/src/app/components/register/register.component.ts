0b2e579213cf375e41b49ddb860eb3e163a96405
Deepesh Khanal <deepesh.khanal@itonics.de>
2020-07-23 20:17:14 +0545

    Added register form

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
 	styleUrls: ['./register.component.scss']
 })
-
 export class RegisterComponent implements OnInit {
+	registerForm: FormGroup;
+	loading = false;
+	submitted = false;
 
-	constructor() { }
+	constructor(
+		private formBuilder: FormBuilder,
+	) { }
 
 	ngOnInit() {
+		this.registerForm = this.formBuilder.group(
+			{
+				firstName: ['', [Validators.required]],
+				lastName: ['', [Validators.required]],
+				userName: ['', [Validators.required, Validators.minLength(4)]],
+				password: ['', [Validators.required, Validators.minLength(8)]],
+				confirmPassword: ['', [Validators.required]],
+				email: ['', [Validators.required]],
+				phoneNumber: ['', [Validators.required]]
+			},
+		);
+	}
 
+	// convenience getter for easy access to form fields
+	get f() {
+		return this.registerForm.controls;
+	}
+
+	onRegisterSubmit() {
+		this.submitted = true;
+
+		// stop here if form is invalid
+		if (this.registerForm.invalid) {
+			return;
+		}
+
+		
+		
+
+		this.loading = true;
 	}
 }
