0a1cb65be488a12ba1d34caa659938a7694cea47
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-07-26 21:57:51 +0545

    Added login form builder

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-login',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+	loginForm: FormGroup;
+	loading = false;
+	submitted = false;
 
 	constructor(
+		private formBuilder: FormBuilder,
 	) { }
 
 	ngOnInit() {
+		this.loginForm = this.formBuilder.group(
+			{
+				userName: ['', [Validators.required, Validators.minLength(4)]],
+				password: ['', [Validators.required, Validators.minLength(8)]],
+			}
+		);
+	}
+
+	// convenience getter for easy access to form fields
+	get f() {
+		return this.loginForm.controls;
+	}
+
+	onLoginSubmit() {
+		this.submitted = true;
+
+		// stop here if form is invalid
+		if (this.loginForm.invalid) {
+			return;
+		}
+
+		this.loading = true;
 	}
 }
