ca8dcb5197cc970f9ebbf6bede1bfba229f4dbdd
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-07-27 20:28:40 +0545

    Added logic login in auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -15,7 +17,18 @@ export class AuthService {
 		return this.http.post(`http://localhost:${this.serverPort}/register`, user);
 	}
 
-	isUnique(field: string, data: string){
-		return this.http.post(`http://localhost:${this.serverPort}/checkunique`, {field: field, data: data});
+	isUnique(field: string, data: string) {
+		return this.http.post(`http://localhost:${this.serverPort}/checkunique`, { field: field, data: data });
+	}
+
+	login(user: any) {
+		return this.http.post<any>(`http://localhost:${this.serverPort}/login`, user).pipe(
+			map((user) => {
+				if (user) {
+					localStorage.setItem('access_token', user.token);
+				}
+				return user;
+			})
+		);
 	}
 }
