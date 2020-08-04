1336348733ee06e0c0ef1c7ab9ff5e36923af002
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-04 20:58:37 +0545

    Added route to dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
 	{path: '', component: WelcomeComponent},
 	{path: 'register', component: RegisterComponent},
 	{path: 'login', component: LoginComponent},
+	{path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
